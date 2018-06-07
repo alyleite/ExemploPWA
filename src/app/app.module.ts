2b2c8650bf11cc0b1d5bdfae620b5bd2b7653862
Daniel Parisotto <danielp.nextage@gmail.com>
2018-06-07 15:19:08 -0300

    Criando cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule, MatCardModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { ImgCardComponent } from './img-card/img-card.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    MatToolbarModule,
+    MatCardModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
